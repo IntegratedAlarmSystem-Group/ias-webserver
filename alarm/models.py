ad6112701cb5872b21948806e4d7267851e81d60
Sebastian <sebafehlandt@gmail.com>
2017-10-25 18:52:10 -0300

    Create Alarm

--- alarm/models.py
+++ alarm/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Alarm(models.Model):
+    value = models.IntegerField(default=0, blank=False, null=False)
+    mode = models.PositiveIntegerField(default=0, blank=False, null=False)
+    core_timestamp = models.PositiveIntegerField(blank=False, null=False)
+    core_id = models.CharField(max_length=100)
+    running_id = models.CharField(max_length=100)
+
+    def __str__(self):
+        """returns a string representation of the object"""
+        return str(self.core_id) + '=' + str(self.value)
--- alarm/tests/tests_models.py
+++ alarm/tests/tests_models.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
+from ..models import Alarm
 
 # Create your tests here.
+class AlarmModelTestCase(TestCase):
+    """This class defines the test suite for the Alarm model tests"""
+
+    def test_create_alarm(self):
+        """Test if we can create an alarm through the models"""
+        # Arrange:
+        self.old_count = Alarm.objects.count()
+        # Act:
+        self.alarm = Alarm.objects.create(
+            value = 1,
+            core_timestamp = 10000,
+            mode = 1,
+            core_id = 'ACS_NC',
+            running_id = 'ANTENNA_DV16$WVR$AMBIENT_TEMPERATURE @ACS_NC',
+        )
+        # Assert:
+        self.new_count = Alarm.objects.count()
+        self.assertEquals(
+            self.old_count + 1,
+            self.new_count,
+            'The Alarm was not created'
+        )
+        self.assertEquals(self.alarm.core_id,
+            'ACS_NC',
+            "The given and saved alarm's core_id differ"
+        )
+
+    def test_delete_alarm(self):
+        """Test if we can delete an alarm through the models"""
+        # Arrange:
+        self.alarm = Alarm.objects.create(
+            value = 1,
+            core_timestamp = 10000,
+            mode = 1,
+            core_id = 'ACS_NC',
+            running_id = 'ANTENNA_DV16$WVR$AMBIENT_TEMPERATURE @ACS_NC',
+        )
+        self.old_count = Alarm.objects.count()
+        # Act:
+        Alarm.objects.filter(core_id = self.alarm.core_id).delete()
+        # Assert:
+        self.new_count = Alarm.objects.count()
+        self.assertEquals(
+            self.old_count - 1,
+            self.new_count,
+            'The Alarm was not deleted'
+        )
