b50cd0443f3ae1cfb3b8ef433d14af78c6607343
Sebastian <sebafehlandt@gmail.com>
2017-10-30 17:18:54 -0300

    Add templates to see the databinding

--- alarms/views.py
+++ alarms/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Alarm
+
 
 # Create your views here.
+def index(request):
+    """
+    Root page view. Just shows a list of alarms currently available.
+    """
+    return render(request, "index.html", {
+        "integer_values": Alarm.objects.order_by("id"),
+    })
--- ias_webserver/settings.py
+++ ias_webserver/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'ias_webserver.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 CHANNEL_LAYERS = {
     "default": {
