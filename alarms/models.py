ea724a30c0866188d424379c3bdf47821c1a229d
Sebastian <sebafehlandt@gmail.com>
2017-11-07 15:17:40 -0300

    Bind all fields of Alarm model

--- alarms/models.py
+++ alarms/models.py
@@ -36,7 +36,7 @@ class Alarm(models.Model):
 class AlarmBinding(WebsocketBinding):
     model = Alarm
     stream = "alarms"
-    fields = ["value", "mode", "core_timestamp", "core_id", "running_id"]
+    fields = "__all__"
 
     @classmethod
     def group_names(cls, *args, **kwargs):
