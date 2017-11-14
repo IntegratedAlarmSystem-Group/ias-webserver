0e81ec01a143eb88ef6578c796e9439831096189
mxolivares <maximiliano.olivares@inria.cl>
2017-11-14 19:26:52 -0300

    Add request consumer. Update related demultiplexer.

--- alarms/consumers.py
+++ alarms/consumers.py
@@ -1,7 +1,31 @@
 # from django.http import HttpResponse
 # from channels.handler import AsgiHandler
-from channels.generic.websockets import WebsocketDemultiplexer
-from .models import AlarmBinding
+from channels.generic.websockets import (
+    WebsocketDemultiplexer,
+    JsonWebsocketConsumer
+)
+from django.core import serializers
+from .models import Alarm, AlarmBinding
+
+
+class AlarmRequestConsumer(JsonWebsocketConsumer):
+
+    def receive(self, content, **kwargs):
+
+        if content is not None:
+
+            if content['action'] == 'list':
+
+                queryset = Alarm.objects.all()
+
+                data = serializers.serialize(
+                    'json',
+                    list(queryset)
+                )
+
+                self.message.reply_channel.send({
+                    "text": data
+                })
 
 
 class AlarmDemultiplexer(WebsocketDemultiplexer):
@@ -13,6 +37,7 @@ class AlarmDemultiplexer(WebsocketDemultiplexer):
     """
     consumers = {
         "alarms": AlarmBinding.consumer,
+        "requests": AlarmRequestConsumer
     }
 
     groups = ["alarms_group"]
