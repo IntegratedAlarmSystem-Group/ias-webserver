e98452d5da4745e24a7d6ce7f03729ac81df2fff
Sebastian <sebafehlandt@gmail.com>
2018-02-16 19:33:51 -0300

    Improve README and requirements, include new routing

--- ias_webserver/routing.py
+++ ias_webserver/routing.py
@@ -1,13 +1,10 @@
 from django.conf.urls import url
 from channels.routing import ProtocolTypeRouter, URLRouter
-from alarms.consumers import CoreConsumer, RequestConsumer, NotifyConsumer
+from alarms.consumers import CoreConsumer, ClientConsumer
 
 application = ProtocolTypeRouter({
-    # Empty for now (http->django views is added by default)
     "websocket": URLRouter([
-        url("^requests/?$", RequestConsumer),
-        url("^notify/?$", NotifyConsumer),
+        url("^stream/?$", ClientConsumer),
         url("^core/?$", CoreConsumer),
     ]),
-
 })
