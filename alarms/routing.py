dd476c543ff32f93d26053f250521f1e79538db7
Sebastian <sebafehlandt@gmail.com>
2018-02-19 11:16:14 -0300

    Remove unused old routing from alarms app

--- alarms/routing.py
+++
@@ -1,8 +0,0 @@
-from channels.routing import route_class
-from .consumers import CoreConsumer, AlarmRequestConsumer
-
-
-channel_routing = [
-    route_class(AlarmRequestConsumer, path='^/stream/?$'),
-    route_class(CoreConsumer, path='^/core/?$')
-]
