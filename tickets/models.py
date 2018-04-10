ddb9df35f55fa5779222bbf0ae4293fe4d81f71c
Jazmine <jamaldonadof@ug.uchile.cl>
2018-04-10 19:42:58 -0300

    Add tickets model and related tests

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.utils import timezone
+from utils.choice_enum import ChoiceEnum
 
-# Create your models here.
+
+class TicketStatus(ChoiceEnum):
+
+    OPEN = 1
+    CLOSE = 0
+
+    @classmethod
+    def options(cls):
+        """ Return a list of tuples with the valid options. """
+        return cls.get_choices()
+
+
+class Ticket(models.Model):
+    """ Ticket associated to an alarm that needs to be acknowledged """
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    """ Time when the ticket is created """
+
+    resolve_at = models.DateTimeField(null=True)
+    """ Time when the ticket is updated """
+
+    alarm_id = models.CharField(max_length=64)
+    """ Reference to the related alarm """
+
+    message = models.CharField(max_length=256, null=True)
+    """ Message posted when the ticket is closed """
+
+    status = models.IntegerField(
+        choices=TicketStatus.options(),
+        default=1,
+    )
+    """ State of the ticket, default is open """
+
+    def resolve(self, message):
+        """ Resolves the ticket modifying the status, the resolution timestamp
+        and the message """
+        if message.strip() is not "" and self.status == 1:
+            self.status = 0
+            self.resolve_at = timezone.now()
+            self.message = message
+            self.save()
+            return "solved"
+        return "ignored"
---
+++ tickets/tests/tests_tickets_models.py
@@ -0,0 +1,65 @@
+from django.test import TestCase
+from tickets.models import Ticket
+from freezegun import freeze_time
+from django.utils import timezone
+
+
+class TicketsModelsTestCase(TestCase):
+
+    def test_create_ticket(self):
+        """ Test if we can create a ticket"""
+        resolution_dt = timezone.now()
+        with freeze_time(resolution_dt):
+            # Act:
+            ticket = Ticket(alarm_id='alarm_id')
+            ticket.save()
+            retrieved_ticket = Ticket.objects.get(alarm_id='alarm_id')
+
+            # Asserts:
+            self.assertEqual(
+                retrieved_ticket.status, 1,
+                'Ticket must be open by default'
+            )
+            self.assertEqual(
+                retrieved_ticket.created_at, resolution_dt,
+                'Ticket was not created with the correct creation timestamp'
+            )
+            self.assertEqual(
+                retrieved_ticket.resolve_at, None,
+                'When the ticket is created the resolve_at time must be none'
+            )
+            self.assertEqual(
+                retrieved_ticket.message, None,
+                'When the ticket is created the message must be none'
+            )
+
+    def test_resolve_a_ticket(self):
+        """ Test if we can resolve a ticket passing it a valid message """
+        # Arrange:
+        ticket = Ticket(alarm_id='alarm_id')
+        ticket.save()
+
+        # Act:
+        resolution_dt = timezone.now()
+        with freeze_time(resolution_dt):
+            response = ticket.resolve(message='This ticket was solved')
+            retrieved_ticket = Ticket.objects.get(alarm_id='alarm_id')
+
+            # Asserts:
+            self.assertEqual(
+                retrieved_ticket.status, 0,
+                'Solved ticket status must be closed (0)'
+            )
+            self.assertEqual(
+                retrieved_ticket.resolve_at, resolution_dt,
+                'When the ticket is solved the resolve_at time must be greater \
+                 than the created_at datetime'
+            )
+            self.assertEqual(
+                retrieved_ticket.message, 'This ticket was solved',
+                'When the ticket is created the message must be none'
+            )
+            self.assertEqual(
+                response, 'solved',
+                'Valid resolution is not solved correctly'
+            )
