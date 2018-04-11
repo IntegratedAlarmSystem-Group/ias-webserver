bbc36f50554dfce55f57829aa8b1fbf3ada95bfd
Sebastian <sebafehlandt@gmail.com>
2018-04-11 12:00:05 -0300

    Create Dockerfile-dev that mounts the project folder

--- runserver-dev.sh
+++ runserver-dev.sh
@@ -8,5 +8,4 @@
 
 # echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@fake-admin.com', 'nimda') if (User.objects.filter(username='admin').exists() == False) else None" | python manage.py shell
 echo "Running with development server!!"
-python manage.py runserver 0.0.0.0:8001 &
-python manage.py broadcaststatus --hostname 0.0.0.0 --port 8001 --rate 5
+python manage.py runserver 0.0.0.0:8001 & python manage.py broadcaststatus --hostname 0.0.0.0 --port 8001 --rate 5
