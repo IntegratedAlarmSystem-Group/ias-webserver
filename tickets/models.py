de912115b947789fe80254b5707460000a675a88
Sebastian <sebafehlandt@gmail.com>
2018-06-14 18:06:53 -0400

    Add ShelveRegistry model

--- tickets/models.py
+++ tickets/models.py
@@ -1,12 +1,17 @@
+from datetime import timedelta
 from django.db import models
 from django.utils import timezone
 from utils.choice_enum import ChoiceEnum
 
 
 class TicketStatus(ChoiceEnum):
+    """ Status options of a Ticket """
 
     ACK = 1
+    """ Acknowledged """
+
     UNACK = 0
+    """ Unacknowledged """
 
     @classmethod
     def options(cls):
@@ -36,7 +41,7 @@ class Ticket(models.Model):
     """ State of the ticket, default is open """
 
     def __str__(self):
-        """ Return a string representation of a ticket """
+        """ Return a string representation of the ticket """
         return str(self.created_at) + ' - ' + self.alarm_id
 
     def to_dict(self):
@@ -62,3 +67,77 @@ class Ticket(models.Model):
         self.message = message
         self.save()
         return "solved"
+
+
+class ShelveRegistryStatus(ChoiceEnum):
+    """ Status options of a ShelveRegistry """
+
+    SHELVED = 1
+    """ Shelved """
+
+    UNSHELVED = 0
+    """ Unshelved """
+
+    @classmethod
+    def options(cls):
+        """ Return a list of tuples with the valid options. """
+        return cls.get_choices()
+
+
+class ShelveRegistry(models.Model):
+    """ Registry of when an alarm is shelved """
+
+    shelved_at = models.DateTimeField(auto_now_add=True)
+    """ Time when the alarm is shelved """
+
+    unshelved_at = models.DateTimeField(null=True)
+    """ Time when the alarm is unshelved """
+
+    alarm_id = models.CharField(max_length=64, db_index=True)
+    """ Reference to the related alarm """
+
+    message = models.CharField(max_length=256, null=False, blank=False)
+    """ Message posted when the ticket is closed """
+
+    timeout = models.DurationField(default=timedelta(hours=2))
+    """ Timeout after which a shelved Alarm must be unshelved """
+
+    status = models.IntegerField(
+        choices=ShelveRegistryStatus.options(),
+        default=int(ShelveRegistryStatus.get_choices_by_name()['SHELVED']),
+    )
+    """ State of the shelve_registry, default is shelved """
+
+    def __str__(self):
+        """ Return a string representation of the shelve_registry """
+        return str(self.created_at) + ' - ' + self.alarm_id
+
+    def save(self, *args, **kwargs):
+        """ Check if the message is not empty before saving """
+        if not self.message or self.message == '':
+            raise ValueError("Registry message cannot be empty")
+        super().save(*args, **kwargs)
+
+    def to_dict(self):
+        """ Return the shelve_registry as a dictionary """
+        return {
+            'shelved_at': self.shelved_at,
+            'unshelved_at': self.unshelved_at,
+            'alarm_id': self.alarm_id,
+            'message': self.message,
+            'status': self.status
+        }
+
+    # def acknowledge(self, message):
+    #     """ Resolves the ticket modifying the status, the resolution timestamp
+    #     and the message """
+    #     if self.status == int(TicketStatus.get_choices_by_name()['ACK']):
+    #         return "ignored-ticket-ack"
+    #     if message.strip() is "":
+    #         return "ignored-wrong-message"
+    #
+    #     self.status = int(TicketStatus.get_choices_by_name()['ACK'])
+    #     self.acknowledged_at = timezone.now()
+    #     self.message = message
+    #     self.save()
+    #     return "solved"
--- tickets/tests/tests_tickets_models.py
+++ tickets/tests/tests_tickets_models.py
@@ -1,7 +1,13 @@
+from datetime import timedelta
 from freezegun import freeze_time
 from django.test import TestCase
 from django.utils import timezone
-from tickets.models import Ticket, TicketStatus
+from tickets.models import (
+    Ticket,
+    TicketStatus,
+    ShelveRegistry,
+    ShelveRegistryStatus
+)
 
 
 class TicketsModelsTestCase(TestCase):
@@ -47,22 +53,99 @@ class TicketsModelsTestCase(TestCase):
             response = ticket.acknowledge(message='This ticket was solved')
             retrieved_ticket = Ticket.objects.get(alarm_id='alarm_id')
 
-            # Asserts:
-            self.assertEqual(
-                retrieved_ticket.status,
-                int(TicketStatus.get_choices_by_name()['ACK']),
-                'Solved ticket status must be closed (0)'
+        # Asserts:
+        self.assertEqual(
+            retrieved_ticket.status,
+            int(TicketStatus.get_choices_by_name()['ACK']),
+            'Solved ticket status must be closed (0)'
+        )
+        self.assertEqual(
+            retrieved_ticket.acknowledged_at, resolution_dt,
+            'When the ticket is solved the acknowledge_at time must be \
+            greater than the created_at datetime'
+        )
+        self.assertEqual(
+            retrieved_ticket.message, 'This ticket was solved',
+            'When the ticket is created the message must be none'
+        )
+        self.assertEqual(
+            response, 'solved',
+            'Valid resolution is not solved correctly'
+        )
+
+
+class ShelveRegistryModelsTestCase(TestCase):
+    """This class defines the test suite for the ShelveRegistry model tests"""
+
+    def test_create_registry(self):
+        """ Test if we can create a shelve_registry"""
+        resolution_dt = timezone.now()
+        with freeze_time(resolution_dt):
+            # Act:
+            registry = ShelveRegistry(
+                alarm_id='alarm_id', message='Shelving because of reasons'
             )
-            self.assertEqual(
-                retrieved_ticket.acknowledged_at, resolution_dt,
-                'When the ticket is solved the acknowledge_at time must be \
-                greater than the created_at datetime'
-            )
-            self.assertEqual(
-                retrieved_ticket.message, 'This ticket was solved',
-                'When the ticket is created the message must be none'
-            )
-            self.assertEqual(
-                response, 'solved',
-                'Valid resolution is not solved correctly'
-            )
+            registry.save()
+            retrieved_reg = ShelveRegistry.objects.get(alarm_id='alarm_id')
+
+        # Asserts:
+        self.assertEqual(
+            retrieved_reg.status,
+            int(ShelveRegistryStatus.get_choices_by_name()['SHELVED']),
+            'Registry must be shelved by default'
+        )
+        self.assertEqual(
+            retrieved_reg.shelved_at, resolution_dt,
+            'Registry was not created with the correct shelved timestamp'
+        )
+        self.assertEqual(
+            retrieved_reg.unshelved_at, None,
+            'When the registry is created the unshelved time must be none'
+        )
+        self.assertEqual(
+            retrieved_reg.message, 'Shelving because of reasons',
+            'When the registry is created the message must not be none'
+        )
+        self.assertEqual(
+            retrieved_reg.timeout, timedelta(hours=2),
+            'The default timeout should be 2 hours'
+        )
+
+    def test_cannot_create_registry_with_no_message(self):
+        """ Test if we cannot create a shelve_registry without a message"""
+        # Act:
+        registry = ShelveRegistry(alarm_id='alarm_id')
+        with self.assertRaises(ValueError):
+            registry.save()
+
+    # def test_acknowledge_a_ticket(self):
+    #     """ Test if we can acknowledge a shelve_registry passing it a valid message """
+    #     # Arrange:
+    #     ticket = Ticket(alarm_id='alarm_id')
+    #     ticket.save()
+    #
+    #     # Act:
+    #     resolution_dt = timezone.now()
+    #     with freeze_time(resolution_dt):
+    #         response = ticket.acknowledge(message='This ticket was solved')
+    #         retrieved_ticket = Ticket.objects.get(alarm_id='alarm_id')
+    #
+    #         # Asserts:
+    #         self.assertEqual(
+    #             retrieved_ticket.status,
+    #             int(TicketStatus.get_choices_by_name()['ACK']),
+    #             'Solved ticket status must be closed (0)'
+    #         )
+    #         self.assertEqual(
+    #             retrieved_ticket.acknowledged_at, resolution_dt,
+    #             'When the ticket is solved the acknowledge_at time must be \
+    #             greater than the created_at datetime'
+    #         )
+    #         self.assertEqual(
+    #             retrieved_ticket.message, 'This ticket was solved',
+    #             'When the ticket is created the message must be none'
+    #         )
+    #         self.assertEqual(
+    #             response, 'solved',
+    #             'Valid resolution is not solved correctly'
+    #         )
