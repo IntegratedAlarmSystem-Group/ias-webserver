f8ccdc36f190e8ca64c426ba8ce2b2e0f7e9cc80
Maximiliano Olivares <mx.olivares.p@gmail.com>
2018-11-08 11:03:57 -0300

    Update cdb api tests with authenticated client. Update tests with valid conditions for authenticated clients

--- cdb/tests/tests_cdb_api.py
+++ cdb/tests/tests_cdb_api.py
@@ -1,11 +1,43 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 from rest_framework import status
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
 from django.urls import reverse
 from cdb.readers import CdbReader
 
 
-class CdbApiTestCase(TestCase):
+class APITestBase:
+
+    def create_user(self, **kwargs):
+        """ Creates a user with selected permissions """
+        permissions = kwargs.get('permissions', [])
+        username = kwargs.get('username', 'user')
+        pwd = kwargs.get('password', 'pwd')
+        email = 'user@user.cl'
+        user = User.objects.create_user(username, password=pwd, email=email)
+        for permission in permissions:
+            user.user_permissions.add(permission)
+        return user
+
+    def authenticate_client_using_token(self, client, token):
+        """ Authenticates a selected API Client using a related User token """
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+
+class CdbApiTestCase(APITestBase, TestCase):
     """Test suite for the api views."""
+
+    def setUp(self):
+        """Define the test suite setup"""
+        self.no_permissions_user = self.create_user(
+            username='user', password='123', permissions=[])
+        self.authenticated_client = APIClient()
+        self.authenticate_client_using_token(
+            self.authenticated_client,
+            Token.objects.get(user__username=self.no_permissions_user)
+        )
+        self.client = self.authenticated_client
 
     def test_api_can_list_ias(self):
         """ Test that the api can retrieve an Ias """
