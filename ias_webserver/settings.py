5790caec569086d96d9cff43e1bba5b1b35813e3
Sebastian <sebafehlandt@gmail.com>
2018-11-15 16:48:31 -0300

    Allow Cors credentials

--- ias_webserver/settings.py
+++ ias_webserver/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'ias_webserver.wsgi.application'
 
 # CORS Configuration
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = False
+CORS_ALLOW_CREDENTIALS = True
 
 # CORS_ORIGIN_WHITELIST = (
 #     '<DOMAIN>[:PORT]',
