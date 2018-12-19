7d2d622b3189bd1d1694b2d16cfaed907f773202
Maximiliano Olivares <mx.olivares.p@gmail.com>
2018-12-19 15:40:25 -0300

    Add test for selected case with alarm dependencies

--- alarms/tests/tests_collections_counter_by_view_cases.py
+++ alarms/tests/tests_collections_counter_by_view_cases.py
@@ -935,3 +935,215 @@ class TestCountPerViewForAlarmsUpdates:
 
         counter = Alarm.objects.counter_by_view()
         assert counter == {'antennas': 0, 'weather': 0}, 'Unexpected count'
+
+
+class TestCountByViewForDependencies:
+    """ Test suite for selected cases of alarms with dependencies """
+
+    def set_mock_views_configuration(self, mocker):
+
+        mock_view_names = ['view']
+
+        mock_alarms_views_dict = {
+            "alarm_parent": ["view"],
+            "alarm_dependency_one": ["view"],
+            "alarm_dependency_two": ["view"],
+        }
+
+        PanelsConnector_get_names_of_views = \
+            mocker.patch.object(
+                PanelsConnector, 'get_names_of_views'
+            )
+        PanelsConnector_get_names_of_views.return_value = mock_view_names
+
+        PanelsConnector_get_alarms_views_dict_of_alarm_configs = \
+            mocker.patch.object(
+                PanelsConnector, 'get_alarms_views_dict_of_alarm_configs'
+            )
+        PanelsConnector_get_alarms_views_dict_of_alarm_configs.return_value = \
+            mock_alarms_views_dict
+
+    def build_alarms(self):
+
+        mock_alarms = {}
+
+        for core_id, value in [
+            ('alarm_parent', 0),
+            ('alarm_dependency_one', 0),
+            ('alarm_dependency_two', 0)
+        ]:
+            alarm = AlarmFactory.build()
+            alarm.core_id = core_id
+            alarm.views = AlarmCollection.alarms_views_dict.get(
+                alarm.core_id, [])
+            alarm.value = value
+            if value == 0:
+                assert alarm.is_set() is False
+            else:
+                assert alarm.is_set() is True
+            assert alarm.ack is False
+            mock_alarms[core_id] = alarm
+
+        mock_alarms['alarm_parent'].dependencies = [
+            mock_alarms['alarm_dependency_one'].core_id,
+            mock_alarms['alarm_dependency_two'].core_id
+        ]
+
+        return mock_alarms
+
+    @pytest.mark.asyncio
+    @pytest.mark.django_db
+    async def test_count_for_alarm_parent_with_SET_ACK_to_SET_UNACK_transition(
+        self, mocker
+    ):
+
+        # Arrange
+        self.set_mock_views_configuration(mocker)
+        AlarmCollection.reset()
+        Alarm.objects.reset_counter_by_view()
+        mock_alarms_dict = self.build_alarms()
+
+        old_timestamp = int(round(time.time() * 1000))
+
+        # Initial counter:
+        counter = Alarm.objects.counter_by_view()
+        assert counter == {}, 'Unexpected count'
+
+        # Act: Create cleared alarms (order required):
+        for alarm_key in [
+            'alarm_dependency_one',
+            'alarm_dependency_two',
+            'alarm_parent'
+        ]:
+            alarm = mock_alarms_dict[alarm_key]
+            status = await AlarmCollection.add_or_update_and_notify(alarm)
+            assert status == 'created-alarm', \
+                'The status must be created-alarm'
+            assert alarm.core_id in AlarmCollection.get_all_as_dict(), \
+                'New alarms should be created'
+            stored_alarm = AlarmCollection.get(alarm.core_id)
+            assert stored_alarm.is_set() is False, """
+                Expected cleared state
+            """
+            # The system should use an ack state for the new cleared alarms
+            # because there are no ack tickets
+            assert AlarmCollection.get(alarm.core_id).ack is True, """
+                Expected ack state
+            """
+
+        # Assert:
+        counter = Alarm.objects.counter_by_view()
+        assert counter == {'view': 0}, 'Unexpected count'
+
+        # Act: Parent and child to set state
+        new_timestamp = old_timestamp + 10
+
+        for alarm_key in [
+            'alarm_dependency_one',
+            'alarm_parent'
+        ]:
+            alarm = AlarmFactory.build()
+            alarm.core_id = mock_alarms_dict[alarm_key].core_id
+            alarm.dependencies = mock_alarms_dict[alarm_key].dependencies
+            alarm.value = 1
+            alarm.core_timestamp = new_timestamp
+            status = await AlarmCollection.add_or_update_and_notify(alarm)
+            assert status == 'updated-alarm', \
+                'The status must be updated-alarm'
+            stored_alarm = AlarmCollection.get(alarm.core_id)
+            assert stored_alarm.ack is False, """
+                Expected unack state
+            """
+            assert stored_alarm.is_set() is True, """
+                Expected set state
+            """
+            assert stored_alarm.views == mock_alarms_dict[alarm_key].views, """
+                Unexpected views
+            """
+
+        # Assert:
+        counter = Alarm.objects.counter_by_view()
+        assert counter == {'view': 2}, 'Unexpected count'
+
+        # Act: Ack child
+        for alarm_key in [
+            'alarm_dependency_one'
+        ]:
+            alarm = mock_alarms_dict[alarm_key]
+            await AlarmCollection.acknowledge(alarm.core_id)
+
+        for alarm_key in [
+            'alarm_dependency_one',
+            'alarm_parent'
+        ]:
+            alarm = mock_alarms_dict[alarm_key]
+            stored_alarm = AlarmCollection.get(alarm.core_id)
+            assert stored_alarm.ack is True, """
+                Expected ack state
+            """
+            assert stored_alarm.is_set() is True, """
+                Expected set state
+            """
+            assert stored_alarm.views == mock_alarms_dict[alarm_key].views, """
+                Unexpected views
+            """
+
+        # Assert:
+        counter = Alarm.objects.counter_by_view()
+        assert counter == {'view': 0}, 'Unexpected count'
+
+        # Act: Other child to set state
+        old_timestamp = new_timestamp
+        new_timestamp = old_timestamp + 10
+
+        for alarm_key in [
+            'alarm_dependency_two'
+        ]:
+            alarm = AlarmFactory.build()
+            alarm.core_id = mock_alarms_dict[alarm_key].core_id
+            alarm.dependencies = mock_alarms_dict[alarm_key].dependencies
+            alarm.value = 1
+            alarm.core_timestamp = new_timestamp
+            status = await AlarmCollection.add_or_update_and_notify(alarm)
+            assert status == 'updated-alarm', \
+                'The status must be updated-alarm'
+            stored_alarm = AlarmCollection.get(alarm.core_id)
+            assert stored_alarm.ack is False, """
+                Expected unack state
+            """
+            assert stored_alarm.is_set() is True, """
+                Expected set state
+            """
+            assert stored_alarm.views == mock_alarms_dict[alarm_key].views, """
+                Unexpected views
+            """
+
+        alarm_key = 'alarm_parent'
+        alarm = mock_alarms_dict[alarm_key]
+        stored_alarm = AlarmCollection.get(alarm.core_id)
+        assert stored_alarm.ack is False, """
+            Expected unack state
+        """
+        assert stored_alarm.is_set() is True, """
+            Expected set state
+        """
+        assert stored_alarm.views == mock_alarms_dict[alarm_key].views, """
+            Unexpected views
+        """
+
+        alarm_key = 'alarm_dependency_one'
+        alarm = mock_alarms_dict[alarm_key]
+        stored_alarm = AlarmCollection.get(alarm.core_id)
+        assert stored_alarm.ack is True, """
+            Expected ack state
+        """
+        assert stored_alarm.is_set() is True, """
+            Expected set state
+        """
+        assert stored_alarm.views == mock_alarms_dict[alarm_key].views, """
+            Unexpected views
+        """
+
+        # Assert:
+        counter = Alarm.objects.counter_by_view()
+        assert counter == {'view': 2}, 'Unexpected count'
