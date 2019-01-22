8dda95c0a0749b542292613658d247f91eeda6c3
Maximiliano Olivares <mx.olivares.p@gmail.com>
2019-01-22 17:43:36 -0300

    Remove unused serializers

--- panels/serializers.py
+++ panels/serializers.py
@@ -1,32 +1,5 @@
 from rest_framework import serializers
-from panels.models import Type, View, AlarmConfig, Placemark
-
-
-class TypeSerializer(serializers.ModelSerializer):
-    """Serializer to map the Model instance into JSON format."""
-
-    class Meta:
-        """Meta class to map serializer's fields with the model fields."""
-        model = Type
-        fields = '__all__'
-
-
-class ViewSerializer(serializers.ModelSerializer):
-    """Serializer to map the Model instance into JSON format."""
-
-    class Meta:
-        """Meta class to map serializer's fields with the model fields."""
-        model = View
-        fields = '__all__'
-
-
-class AlarmConfigSerializer(serializers.ModelSerializer):
-    """Serializer to map the Model instance into JSON format."""
-
-    class Meta:
-        """Meta class to map serializer's fields with the model fields."""
-        model = AlarmConfig
-        fields = '__all__'
+from panels.models import Placemark
 
 
 class PlacemarkSerializer(serializers.ModelSerializer):
