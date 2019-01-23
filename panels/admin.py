a17696c35f09c695e4bbf74d644ac5c01c6efcb7
Maximiliano Olivares <mx.olivares.p@gmail.com>
2019-01-23 10:17:36 -0300

    Remove AlarmConfig, View and Type models form the admin

--- panels/admin.py
+++ panels/admin.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from panels.models import (
-    View, Type, AlarmConfig, Placemark, PlacemarkType, PlacemarkGroup
+    Placemark, PlacemarkType, PlacemarkGroup
 )
 
 
-admin.site.register(AlarmConfig)
-admin.site.register(View)
-admin.site.register(Type)
 admin.site.register(PlacemarkType)
 admin.site.register(Placemark)
 admin.site.register(PlacemarkGroup)
