e8b111c73d0ece7378fe8aaff5417ea55e6bd189
Sebastian <sebafehlandt@gmail.com>
2019-04-23 16:57:14 -0400

    Disable pagination in tests

--- ias_webserver/settings.py
+++ ias_webserver/settings.py
@@ -248,10 +248,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 1000
+    )
 }
+
+if not os.environ.get('TESTING', False):
+    REST_FRAMEWORK['DEFAULT_PAGINATION_CLASS'] = 'rest_framework.pagination.PageNumberPagination'
+    REST_FRAMEWORK['PAGE_SIZE'] = 1000
 
 ASGI_APPLICATION = "ias_webserver.routing.application"
 
