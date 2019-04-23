30779b5a31b2ff2d9ba37b6dfedd0f3cda2371f0
Sebastian <sebafehlandt@gmail.com>
2019-04-23 16:23:56 -0400

    Add pagination to Django model viewsets

--- ias_webserver/settings.py
+++ ias_webserver/settings.py
@@ -248,7 +248,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1000
 }
 
 ASGI_APPLICATION = "ias_webserver.routing.application"
